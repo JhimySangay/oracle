b250e36c82314e107423667513277775add23c63
Jhimy Rafael Sangay Vega <jhimysangay@gmail.com>
2018-06-03 17:45:47 -0500

    Ejercicio 9

--- D80190GC11_ag.sql
+++ D80190GC11_ag.sql
@@ -1,6 +1,40 @@
 --Ejercicios
 --5 b
-SELECT E.EMPLOYEE_ID,E.LAST_NAME, 
-E.JOB_ID, E.HIRE_DATE "STARTDATE" FROM EMPLOYEES E;
+SELECT
+    e.employee_id,
+    e.last_name,
+    e.job_id,
+    e.hire_date "STARTDATE"
+FROM
+    employees e;
 --6
-SELECT DISTINCT JOB_ID FROM EMPLOYEES;
+
+SELECT DISTINCT
+    job_id
+FROM
+    employees;
+--7
+
+SELECT
+    e.employee_id "Emp #",
+    e.last_name "Employee",
+    e.job_id "Job",
+    e.hire_date "Hire Date"
+FROM
+    employees e;
+--8
+
+SELECT
+    last_name
+    || ', '
+    || job_id
+FROM
+    employees;
+--9
+
+DESC employees;
+
+SELECT employee_id||','|| first_name||','|| last_name||','|| email|| ','|| phone_number
+       ||','|| hire_date|| ','|| job_id|| ','|| salary||','|| commission_pct
+       ||','|| manager_id|| ','|| department_id THE_OUTPUT
+FROM employees
